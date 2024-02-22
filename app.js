c74db0eb5da34756a4ae855d621dea0de90ea6ea
Abdul-Manan-456 <abdulmanan7733@gmail.com>
2024-02-22 15:12:27 +0500

    daf

--- app.js
+++ app.js
@@ -19,17 +19,7 @@ app.use(helmet());
 //   );
 //   next();
 // });
-app.use((req, res, next) => {
-  //allow access from every, elminate CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.removeHeader("x-powered-by");
-  //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  //headers clients can use in their requests
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //allow request to continue and be handled by routes
-  next();
-});
+
 // const corsOpts = {
 //   origin: "*",
 
--- middlewares/Base.js
+++ middlewares/Base.js
@@ -23,6 +23,17 @@ class Base {
     app.use(bodyParser.urlencoded({ limit: "5mb", extended: false }));
     app.use(cookieParser());
     app.use(express.static("public"));
+    app.use((req, res, next) => {
+      //allow access from every, elminate CORS
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.removeHeader("x-powered-by");
+      //set the allowed HTTP methods to be requested
+      res.setHeader("Access-Control-Allow-Methods", "GET");
+      //headers clients can use in their requests
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      //allow request to continue and be handled by routes
+      next();
+    });
     // app.use((req, res, next) => {
     //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     //   res.setHeader(
